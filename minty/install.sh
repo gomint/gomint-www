7f4826cea0f634d805c1c32449abc62d95a45f7b
Felix Scholz <chapldupez@gmail.com>
2020-12-01 02:51:31 +0100

    Initialize Vue project

--- minty/install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-[[ -d ~/.minty/ ]] || mkdir ~/.minty/
-[[ -f ~/.minty/minty ]] && rm -f ~/.minty/minty
-curl https://gomint.io/minty/linux_x64 -o ~/.minty/minty
-chmod +x ~/.minty/minty && echo "PATH=\"~/.minty/:\$PATH\"" >> ~/.profile && export PATH="~/.minty/:$PATH"
